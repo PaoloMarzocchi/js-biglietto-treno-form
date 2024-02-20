3458c555864777298a9c0dc491e7d95e7d19ab10
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-20 15:10:43 +0100

    add inputs into variables and an if/else if function to calculate ticket price in js file

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const fullName = document.getElementById('full_name').value; 
+const userAge = Number(document.getElementById('age').value);
+const distance = Number(document.getElementById('km').value);
+//console.log(fullName);
+//console.log(userAge);
+//console.log(distance);
+const price_per_km = 0.21;
+
+let ticketPrice = distance * price_per_km;
+const discountYoung = ticketPrice * 0.2;
+const discountOld = ticketPrice * 0.4;
+
+if (userAge < 18) {
+    ticketPrice = ticketPrice - discountYoung;
+} else if (userAge > 65) {
+    ticketPrice = ticketPrice - discountOld;
+}
+
+console.log(ticketPrice.toFixed(2) + ' €');
