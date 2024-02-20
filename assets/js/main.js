aa7ce262bcde52314211dc5b3630f4958682a464
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-20 17:07:52 +0100

    add ticket output in js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,34 @@
 const fullName = document.getElementById('full_name').value; 
 const userAge = Number(document.getElementById('age').value);
 const distance = Number(document.getElementById('km').value);
-//console.log(fullName);
-//console.log(userAge);
-//console.log(distance);
+console.log(fullName);
+console.log(userAge);
+console.log(distance);
 const price_per_km = 0.21;
 
 let ticketPrice = distance * price_per_km;
 const discountYoung = ticketPrice * 0.2;
 const discountOld = ticketPrice * 0.4;
+let ticketType = 'Biglietto Standard';
 
 if (userAge < 18) {
     ticketPrice = ticketPrice - discountYoung;
+    ticketType = 'Biglietto Junior';
 } else if (userAge > 65) {
     ticketPrice = ticketPrice - discountOld;
+    ticketType = 'Biglietto Senior';
 }
-ticketPrice = ticketPrice.toFixed(2) + ' €'
+ticketPrice = ticketPrice.toFixed(2) + ' €';
+
+
 const submit = document.querySelector('.btn');
 
 submit.addEventListener('click', function () {
     document.querySelector('.print_name').innerHTML += fullName;
-    //document.querySelector('.print_age').innerHTML += userAge;
+    document.querySelector('.ticket_type').innerHTML += ticketType;
+    document.querySelector('.cabin').innerHTML += Math.floor(Math.random() * 10) + 1;;
+    document.querySelector('.cp').innerHTML += Math.floor(Math.random() * 100000) + 1;;
     document.querySelector('.print_price').innerHTML += ticketPrice ;
-})
+}, 'once')
 
-console.log(ticketPrice.toFixed(2) + ' €');
+
